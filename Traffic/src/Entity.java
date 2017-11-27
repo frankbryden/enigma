fb447c330d3afcf8309976eb318d6e1acebdd2ce
Frank Bryden <frank.bryden@gmail.com>
2017-11-27 11:32:25 +0000

    Added text file solutions

---
+++ Traffic/src/Entity.java
@@ -0,0 +1,6 @@
+import java.awt.Graphics2D;
+
+public abstract class Entity {
+    public abstract void draw(Graphics2D g);
+    public abstract void update();
+}
---
+++ Traffic/src/TrafficRender.java
@@ -0,0 +1,20 @@
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+
+public class TrafficRender extends JPanel{
+
+    public TrafficRender(){
+
+    }
+
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+
+    }
+
+}
